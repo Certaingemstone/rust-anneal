b11a17425c45cbc327eb232957749a9dc950cfb5
Certaingemstone <jade.chongsathapornpong@gmail.com>
2022-06-08 11:22:18 -0400

    Remove visualization temporarily

--- src/main.rs
+++ src/main.rs
@@ -3,96 +3,92 @@ mod sim;
 use sim::lattice::Lattice;
 use std::{io, thread};
 use std::time::Duration;
-use flo_draw::*;
 
 fn main() {
-    with_2d_graphics(|| {
-        // simulation parameters
-        let mut buffer = String::new();
-        let mut stdin = io::stdin();
+    // simulation parameters
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
 
-        // println!("Enter initial temperature");
-        // stdin.read_line(&mut buffer).expect("failed to read input");
-        // let ti: f32 = buffer.trim().parse().unwrap();
-        // buffer.clear();
-        let ti: f32 = 1.0;
+    // println!("Enter initial temperature");
+    // stdin.read_line(&mut buffer).expect("failed to read input");
+    // let ti: f32 = buffer.trim().parse().unwrap();
+    // buffer.clear();
+    let ti: f32 = 1.0;
 
-        println!("Enter temperature step (t starts at 1.0)");
-        stdin.read_line(&mut buffer).expect("failed to read input");
-        let step: f32 = buffer.trim().parse().unwrap();
-        buffer.clear();
+    println!("Enter temperature step (t starts at 1.0)");
+    stdin.read_line(&mut buffer).expect("failed to read input");
+    let step: f32 = buffer.trim().parse().unwrap();
+    buffer.clear();
 
-        let tf = step * 3.0 + 0.005;
+    let tf = step * 3.0 + 0.005;
 
-        println!("Choose lattice size");
-        stdin.read_line(&mut buffer).expect("failed to read input");
-        let dim: usize = buffer.trim().parse().unwrap();
-        buffer.clear();
+    println!("Choose lattice size");
+    stdin.read_line(&mut buffer).expect("failed to read input");
+    let dim: usize = buffer.trim().parse().unwrap();
+    buffer.clear();
 
-        // initialize lattice
-        println!("Choose lattice type: square, tri");
-        stdin.read_line(&mut buffer).expect("failed to read input");
-        let mut lat: Lattice;
-        match buffer.trim() {
-            "square" => {
-                lat = Lattice::square(dim);
-            }
-            "tri" => {
-                lat = Lattice::triangular(dim);
-            }
-            _ => { panic!(); }
+    // initialize lattice
+    println!("Choose lattice type: square, tri");
+    stdin.read_line(&mut buffer).expect("failed to read input");
+    let mut lat: Lattice;
+    match buffer.trim() {
+        "square" => {
+            lat = Lattice::square(dim);
         }
-        buffer.clear();
-        
-        println!("Choose lattice fill proportion (0,1)");
-        stdin.read_line(&mut buffer).expect("failed to read input");
-        let fill: f32 = buffer.trim().parse().unwrap();
-        assert_eq!(fill > 0.0, true);
-        assert_eq!(fill < 1.0, true);
-        lat.fill_random(fill);
-        buffer.clear();
-
-        println!("Animate? (y/n)");
-        stdin.read_line(&mut buffer).expect("failed to read input");
-        let draw: bool;
-        match buffer.trim() {
-            "y" => {
-                draw = true;
-            }
-            _ => {
-                draw = false;
-            }
+        "tri" => {
+            lat = Lattice::triangular(dim);
         }
+        _ => { panic!(); }
+    }
+    buffer.clear();
+    
+    println!("Choose lattice fill proportion (0,1)");
+    stdin.read_line(&mut buffer).expect("failed to read input");
+    let fill: f32 = buffer.trim().parse().unwrap();
+    assert_eq!(fill > 0.0, true);
+    assert_eq!(fill < 1.0, true);
+    lat.fill_random(fill);
+    buffer.clear();
 
-        // simulate and draw
-        assert_eq!(ti > tf, true);
-        assert_eq!(step > 0.0, true);
-        let mut t = ti;
-        let mut rng = rand::thread_rng();
-        let canvas = create_drawing_window("lattice");
-        let mut i = 0;
-        sim::init_draw(&mut lat, &canvas);
-        if draw {
-            while t > tf {
-                sim::step_simulation(&mut lat, &mut rng, &t);
-                t -= step;
-                i += 1;
-                if i % dim == 0 {
-                    sim::update_draw(&mut lat, &canvas);
-                    thread::sleep(Duration::from_millis(1000));
-                    println!("Temperature: {}", t);
-                }
-            }
-        } else {
-            while t > tf {
-                sim::step_simulation(&mut lat, &mut rng, &t);
-                t -= step;
+    println!("Animate? (y/n)");
+    stdin.read_line(&mut buffer).expect("failed to read input");
+    let draw: bool;
+    match buffer.trim() {
+        "y" => {
+            draw = true;
+        }
+        _ => {
+            draw = false;
+        }
+    }
+
+    // simulate and draw
+    assert_eq!(ti > tf, true);
+    assert_eq!(step > 0.0, true);
+    let mut t = ti;
+    let mut rng = rand::thread_rng();
+    // let canvas = create_drawing_window("lattice");
+    let mut i = 0;
+    // sim::init_draw(&mut lat, &canvas);
+    if draw {
+        while t > tf {
+            sim::step_simulation(&mut lat, &mut rng, &t);
+            t -= step;
+            i += 1;
+            if i % dim == 0 {
+                // sim::update_draw(&mut lat, &canvas);
+                // thread::sleep(Duration::from_millis(1000));
                 println!("Temperature: {}", t);
             }
-            sim::update_draw(&mut lat, &canvas);
         }
+    } else {
+        while t > tf {
+            sim::step_simulation(&mut lat, &mut rng, &t);
+            t -= step;
+            println!("Temperature: {}", t);
+        }
+        // sim::update_draw(&mut lat, &canvas);
+    }
         
-        
-        println!("Done!")
-    });
+    println!("Done!")
 }
--- src/sim/mod.rs
+++ src/sim/mod.rs
@@ -1,118 +1,84 @@
 pub mod lattice;
 
 use std::collections::HashSet;
-use flo_draw::*;
-use flo_canvas::*;
+// use flo_draw::*;
+// use flo_canvas::*;
 use lattice::{Lattice, Point};
 
-pub fn init_draw(lat: &mut lattice::Lattice, canvas: &flo_canvas::DrawingTarget) {
-    let bounds: [i16; 4] = lat.boundaries();
-    let pts: Vec<Point> = lat.points.clone();
-    let occ: HashSet<usize> = lat.occupied.set.clone();
-    let adj: Vec<Vec<usize>> = lat.adjacency.clone();
-    let scale = 1000.0 / *bounds.iter().max().unwrap() as f32;
-    let sbounds: Vec<f32> = bounds.iter().map(|x| *x as f32 * scale).collect();
-    let radius = 15.0;
+// pub fn init_draw(lat: &mut lattice::Lattice, canvas: &flo_canvas::DrawingTarget) {
+//     let bounds: [i16; 4] = lat.boundaries();
+//     let pts: Vec<Point> = lat.points.clone();
+//     let occ: HashSet<usize> = lat.occupied.set.clone();
+//     let adj: Vec<Vec<usize>> = lat.adjacency.clone();
+//     let scale = 1000.0 / *bounds.iter().max().unwrap() as f32;
+//     let sbounds: Vec<f32> = bounds.iter().map(|x| *x as f32 * scale).collect();
+//     let radius = 15.0;
 
-    canvas.draw(|gc| {
-        gc.clear_canvas(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-        gc.canvas_height(1100.0);
-        gc.center_region(sbounds[0] - 50.0, sbounds[2] - 50.0, sbounds[1] + 50.0, sbounds[3] + 50.0);
-        gc.stroke_color(Color::Rgba(1.0, 1.0, 1.0, 1.0));
-        gc.line_width(3.0);
+//     canvas.draw(|gc| {
+//         gc.clear_canvas(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+//         gc.canvas_height(1100.0);
+//         gc.center_region(sbounds[0] - 50.0, sbounds[2] - 50.0, sbounds[1] + 50.0, sbounds[3] + 50.0);
+//         gc.stroke_color(Color::Rgba(1.0, 1.0, 1.0, 1.0));
+//         gc.line_width(3.0);
 
-        // draw each connection in the lattice
-        let mut i = 0;
-        for avec in adj {
-            let x0 = scale * pts[i].x as f32; 
-            let y0 = scale * pts[i].y as f32;
-            for a in avec {
-                gc.new_path();
-                gc.move_to(x0, y0);
-                gc.line_to(scale * pts[a].x as f32, scale * pts[a].y as f32);
-                gc.stroke();
-            }
-            i += 1;
-        }
+//         // draw each connection in the lattice
+//         let mut i = 0;
+//         for avec in adj {
+//             let x0 = scale * pts[i].x as f32; 
+//             let y0 = scale * pts[i].y as f32;
+//             for a in avec {
+//                 gc.new_path();
+//                 gc.move_to(x0, y0);
+//                 gc.line_to(scale * pts[a].x as f32, scale * pts[a].y as f32);
+//                 gc.stroke();
+//             }
+//             i += 1;
+//         }
 
-        // draw each site in the lattice
-        i = 0;
-        for p in pts {
-            gc.new_path();
-            gc.circle(p.x as f32 * scale, p.y as f32 * scale, radius);
-            if occ.contains(&i) {
-                gc.fill_color(Color::Rgba(0.3, 1.0, 0.3, 1.0));
-            } else {
-                gc.fill_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-            }  
-            gc.fill();
-            gc.stroke();
-            i += 1;
-        }
+//         // draw each site in the lattice
+//         i = 0;
+//         for p in pts {
+//             gc.new_path();
+//             gc.circle(p.x as f32 * scale, p.y as f32 * scale, radius);
+//             if occ.contains(&i) {
+//                 gc.fill_color(Color::Rgba(0.3, 1.0, 0.3, 1.0));
+//             } else {
+//                 gc.fill_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+//             }  
+//             gc.fill();
+//             gc.stroke();
+//             i += 1;
+//         }
         
-    });
-}
+//     });
+// }
 
-pub fn update_draw(lat: &mut Lattice, canvas: &flo_canvas::DrawingTarget) {
-    let bounds = lat.boundaries(); // xmin, xmax, ymin, ymax
-    let pts: Vec<Point> = lat.points.clone();
-    let occ: HashSet<usize> = lat.occupied.set.clone();
+// pub fn update_draw(lat: &mut Lattice, canvas: &flo_canvas::DrawingTarget) {
+//     let bounds = lat.boundaries(); // xmin, xmax, ymin, ymax
+//     let pts: Vec<Point> = lat.points.clone();
+//     let occ: HashSet<usize> = lat.occupied.set.clone();
 
-    let scale = 1000.0 / *bounds.iter().max().unwrap() as f32;
-    let radius = 15.0;
+//     let scale = 1000.0 / *bounds.iter().max().unwrap() as f32;
+//     let radius = 15.0;
 
-    canvas.draw(|gc| {
-        // refill sites
-        let mut i = 0;
-        for p in pts {
-            gc.new_path();
-            gc.circle(p.x as f32 * scale, p.y as f32 * scale, radius);
-            if occ.contains(&i) {
-                gc.fill_color(Color::Rgba(0.3, 1.0, 0.3, 1.0));
-            } else {
-                gc.fill_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-            }  
-            gc.fill();
-            i += 1;
-        }       
-    });
-}
+//     canvas.draw(|gc| {
+//         // refill sites
+//         let mut i = 0;
+//         for p in pts {
+//             gc.new_path();
+//             gc.circle(p.x as f32 * scale, p.y as f32 * scale, radius);
+//             if occ.contains(&i) {
+//                 gc.fill_color(Color::Rgba(0.3, 1.0, 0.3, 1.0));
+//             } else {
+//                 gc.fill_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+//             }  
+//             gc.fill();
+//             i += 1;
+//         }       
+//     });
+// }
 
 pub fn step_simulation<R>(lattice: &mut Lattice, rng: &mut R, temp: &f32) 
 where R: rand::Rng + ?Sized { 
     lattice.perform_move(rng, temp);
-}
-
-#[allow(dead_code)]
-pub fn test_draw() {
-    with_2d_graphics(|| {
-        let canvas = create_drawing_window("Hello");
-
-        canvas.draw(|gc| {
-            gc.clear_canvas(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-            gc.canvas_height(1000.0);
-            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
-
-            // Draw a rectangle...
-            gc.new_path();
-            gc.move_to(0.0, 0.0);
-            gc.line_to(1000.0, 0.0);
-            gc.line_to(1000.0, 1000.0);
-            gc.line_to(0.0, 1000.0);
-            gc.line_to(0.0, 0.0);
-
-            gc.fill_color(Color::Rgba(1.0, 1.0, 0.8, 1.0));
-            gc.fill();
-
-            // Draw a triangle on top
-            gc.new_path();
-            gc.move_to(200.0, 200.0);
-            gc.line_to(800.0, 200.0);
-            gc.line_to(500.0, 800.0);
-            gc.line_to(200.0, 200.0);
-
-            gc.fill_color(Color::Rgba(0.0, 0.0, 0.8, 1.0));
-            gc.fill();
-        });
-    });
 }
